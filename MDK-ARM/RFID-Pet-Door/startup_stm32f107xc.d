8f327138d065337a9a76e80888b91f3c08ee34f6
ChiChaChai <chatchaishaetan@gmail.com>
2018-04-10 12:09:53 +0700

    add rfid reader

---
+++ MDK-ARM/RFID-Pet-Door/startup_stm32f107xc.d
@@ -0,0 +1,1 @@
+rfid-pet-door\startup_stm32f107xc.o: startup_stm32f107xc.s
---
+++ Src/main.c
@@ -0,0 +1,481 @@
+/**
+  ******************************************************************************
+  * File Name          : main.c
+  * Description        : Main program body
+  ******************************************************************************
+  *
+  * COPYRIGHT(c) 2018 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f1xx_hal.h"
+
+/* USER CODE BEGIN Includes */
+#include "rc522.h"
+/* USER CODE END Includes */
+
+/* Private variables ---------------------------------------------------------*/
+SPI_HandleTypeDef hspi1;
+
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+/* Private variables ---------------------------------------------------------*/
+
+uint8_t 	k;
+uint8_t 	i;
+uint8_t 	j;
+uint8_t 	b;
+uint8_t 	q;
+uint8_t 	en;
+uint8_t 	ok;
+uint8_t 	comand;
+uint8_t		text1[63] = "STM32F103 Mifare RC522 RFID Card reader 13.56 MHz for KEIL HAL\r";
+uint8_t		text2[9] = "Card ID: ";
+uint8_t		end[1] = "\r";
+uint8_t		txBuffer[18] = "Card ID: 00000000\r";
+uint8_t 	retstr[10];
+uint8_t 	rxBuffer[8];
+uint8_t		lastID[4];
+uint8_t		memID[8] = "9C55A1B5";
+uint8_t		str[MFRC522_MAX_LEN];																						// MFRC522_MAX_LEN = 16
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+void Error_Handler(void);
+static void MX_GPIO_Init(void);
+static void MX_SPI1_Init(void);
+static void MX_USART2_UART_Init(void);
+
+/* USER CODE BEGIN PFP */
+/* Private function prototypes -----------------------------------------------*/
+
+// RC522
+uint8_t MFRC522_Check(uint8_t* id);
+uint8_t MFRC522_Compare(uint8_t* CardID, uint8_t* CompareID);
+void MFRC522_WriteRegister(uint8_t addr, uint8_t val);
+uint8_t MFRC522_ReadRegister(uint8_t addr);
+void MFRC522_SetBitMask(uint8_t reg, uint8_t mask);
+void MFRC522_ClearBitMask(uint8_t reg, uint8_t mask);
+uint8_t MFRC522_Request(uint8_t reqMode, uint8_t* TagType);
+uint8_t MFRC522_ToCard(uint8_t command, uint8_t* sendData, uint8_t sendLen, uint8_t* backData, uint16_t* backLen);
+uint8_t MFRC522_Anticoll(uint8_t* serNum);
+void MFRC522_CalulateCRC(uint8_t* pIndata, uint8_t len, uint8_t* pOutData);
+uint8_t MFRC522_SelectTag(uint8_t* serNum);
+uint8_t MFRC522_Auth(uint8_t authMode, uint8_t BlockAddr, uint8_t* Sectorkey, uint8_t* serNum);
+uint8_t MFRC522_Read(uint8_t blockAddr, uint8_t* recvData);
+uint8_t MFRC522_Write(uint8_t blockAddr, uint8_t* writeData);
+void MFRC522_Init(void);
+void MFRC522_Reset(void);
+void MFRC522_AntennaOn(void);
+void MFRC522_AntennaOff(void);
+void MFRC522_Halt(void);
+/* USER CODE END PFP */
+
+/* USER CODE BEGIN 0 */
+
+// string hex to char number
+uint8_t hex_to_char(uint8_t data) {
+	uint8_t number;
+	
+	if (rxBuffer[data] < 58) number = (rxBuffer[data]-48)*16; else number = (rxBuffer[data]-55)*16;
+	data++;
+	if (rxBuffer[data] < 58) number = number+(rxBuffer[data]-48); else number = number+(rxBuffer[data]-55);
+	return number;
+}
+
+// char number to string hex (FF) (Only big letters!)
+void char_to_hex(uint8_t data) {
+	uint8_t digits[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+	
+	if (data < 16) {
+		retstr[0] = '0';
+		retstr[1] = digits[data];
+	} else {
+		retstr[0] = digits[(data & 0xF0)>>4];
+		retstr[1] = digits[(data & 0x0F)];
+	}
+}
+
+// Number to string (16 bit)
+void StrTo() {
+	uint32_t	number;
+	uint32_t 	ret;
+	
+	number = 0;
+	ret = 0;
+	ret = retstr[0]-0x30;
+	number = ret*1000000;
+	ret = retstr[1]-0x30;
+	number = number+ret*100000;
+	ret = retstr[2]-0x30;
+	number = number+ret*10000;
+	ret = retstr[3]-0x30;
+	number = number+ret*1000;
+	ret = retstr[4]-0x30;
+	number = number+ret*100;
+	ret = retstr[5]-0x30;
+	number = number+ret*10;
+	ret = retstr[6]-0x30;
+	number = number+ret;
+}
+
+// String to number (32 bit)
+void ToStr(uint32_t number) {
+	uint32_t i;
+	
+	i = number/1000000000;
+	number = number-i*1000000000;
+	retstr[0] = i+0x30;
+	
+	i=number/100000000;
+	number=number-i*100000000;
+	retstr[1] = i+0x30;
+	
+	i = number/10000000;
+	number = number-i*10000000;
+	retstr[2] = i+0x30;
+	
+	i = number/1000000;
+	number = number-i*1000000;
+	retstr[3] = i+0x30;
+	
+	i = number/100000;
+	number = number-i*100000;
+	retstr[4] = i+0x30;
+	
+	i = number/10000;
+	number = number-i*10000;
+	retstr[5] = i+0x30;
+	
+	i = number/1000;
+	number = number-i*1000;
+	retstr[6] = i+0x30;
+	
+	i = number/100;
+	number = number-i*100;
+	retstr[7] = i+0x30;
+	
+	i = number/10;
+	number = number-i*10;
+	retstr[8] = i+0x30;
+	
+	retstr[9] = number+0x30;
+}
+
+void led(uint8_t n) {
+	for (uint8_t i=0; i<n; i++) {
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);			          // LED1 ON
+		HAL_Delay(100);
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);				          // LED1 OFF
+		HAL_Delay(100);
+	}
+}
+/* USER CODE END 0 */
+
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration----------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_SPI1_Init();
+  MX_USART2_UART_Init();
+
+  /* USER CODE BEGIN 2 */
+	MFRC522_Init();
+	HAL_UART_Transmit(&huart2, text1, 63, 100);
+	HAL_UART_Receive_IT(&huart2,(uint8_t*)rxBuffer, 1);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+  /* USER CODE END WHILE */
+
+  /* USER CODE BEGIN 3 */
+
+		if (!MFRC522_Request(PICC_REQIDL, str)) {
+			if (!MFRC522_Anticoll(str)) {
+				j = 0;
+				q = 0;
+				b = 9;
+				en = 1;
+				char test[16]="Detected!\n";
+				HAL_UART_Transmit(&huart2,(uint8_t*)test,16,100);
+				for (i=0; i<4; i++) if (lastID[i] != str[i]) j = 1;								// Repeat test
+				
+				if (j && en) {
+					q = 0;
+					en = 0;
+					for (i=0; i<4; i++) lastID[i] = str[i];
+					//HAL_UART_Transmit(&huart1, text2, 9, 100);				
+					for (i=0; i<4; i++) {
+						char_to_hex(str[i]);
+						txBuffer[b] = retstr[0];
+						b++;
+						txBuffer[b] = retstr[1];
+						b++;
+						//ToStr(str[i]);
+						//HAL_UART_Transmit(&huart1, retstr, 10, 100);
+					}										
+					//HAL_UART_Transmit(&huart1, end, 1, 100);
+					HAL_UART_Transmit(&huart2, txBuffer, 18, 100);
+					
+					
+					ok = 1;
+					for (i=0; i<8; i++) if (txBuffer[9+i] != memID[i]) ok = 0;
+//					led(1);
+				}
+				
+//				led(1);
+			}
+		}
+		
+		if (ok == 1) {
+			ok = 0;
+//			for (i=0; i<10; i++) {
+//				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);			         	// LED2 ON
+//				HAL_Delay(50);
+//				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET);				     	// LED2 OFF
+//				HAL_Delay(100);
+//			}
+		}
+
+		if (huart2.RxXferCount == 0) {
+//			led(1);
+			comand = rxBuffer[0];
+			if (comand == '1') {
+//				led(1);
+				HAL_UART_Transmit(&huart2, txBuffer, 18, 100);
+				
+			}
+			HAL_UART_Receive_IT(&huart2, (uint8_t*)rxBuffer, 1);
+		}
+		
+	//	if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_1) == GPIO_PIN_RESET) {
+	//		HAL_UART_Transmit(&huart2, txBuffer, 18, 100);
+//			CDC_Transmit_FS(txBuffer, 18);
+//			led(1);
+	//		while (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_1) == GPIO_PIN_RESET);
+//			HAL_Delay(100);
+//		}
+		
+		q++;
+		if (!q) {
+			en = 1;																															// Delay against scan kode
+			for (i=0; i<4; i++) lastID[i] = 0;																	// Delay reading the same card 3s
+		}
+		HAL_Delay(5);
+  }
+  /* USER CODE END 3 */
+
+}
+
+/** System Clock Configuration
+*/
+void SystemClock_Config(void)
+{
+
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = 16;
+  RCC_OscInitStruct.Prediv1Source = RCC_PREDIV1_SOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL8;
+  RCC_OscInitStruct.PLL2.PLL2State = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV4;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+    /**Configure the Systick interrupt time 
+    */
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+    /**Configure the Systick 
+    */
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+    /**Configure the Systick interrupt time 
+    */
+  __HAL_RCC_PLLI2S_ENABLE();
+
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+/* SPI1 init function */
+static void MX_SPI1_Init(void)
+{
+
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_MASTER;
+  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+}
+
+/* USART2 init function */
+static void MX_USART2_UART_Init(void)
+{
+
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+}
+
+/** Configure pins as 
+        * Analog 
+        * Input 
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+static void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PA4 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler */
+  /* User can add his own implementation to report the HAL error return state */
+  while(1) 
+  {
+  }
+  /* USER CODE END Error_Handler */ 
+}
+
+#ifdef USE_FULL_ASSERT
+
+/**
+   * @brief Reports the name of the source file and the source line number
+   * where the assert_param error has occurred.
+   * @param file: pointer to the source file name
+   * @param line: assert_param error line source number
+   * @retval None
+   */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+    ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+
+}
+
+#endif
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
